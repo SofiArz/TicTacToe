7a3e35f49bc158e2b0f48c6b5c0a982b9a552a1d
SofiArz <sofia.arzamendia@hotmail.es>
2020-03-24 18:13:05 -0300

    Creación proyecto TicTacToe

---
+++ src/main/java/ar/com/playmedia/App.java
@@ -0,0 +1,13 @@
+package ar.com.playmedia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
